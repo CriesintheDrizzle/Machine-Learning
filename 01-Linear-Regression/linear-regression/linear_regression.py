5c879683969125e2159aa4c05634e02a5ff54803
binary <2959730117@qq.com>
2024-01-04 11:26:19 +0800

    change

--- 01-Linear-Regression/linear-regression/linear_regression.py
+++ 01-Linear-Regression/linear-regression/linear_regression.py
@@ -17,23 +17,27 @@ class LinearRegression:
          features_deviation)  = prepare_for_training(data, polynomial_degree, sinusoid_degree,normalize_data=True)
          
         self.data = data_processed # 使用预处理之后的数据
-        self.labels = labels
-        self.features_mean = features_mean
-        self.features_deviation = features_deviation
-        self.polynomial_degree = polynomial_degree
-        self.sinusoid_degree = sinusoid_degree
-        self.normalize_data = normalize_data
+        self.labels = labels # 标签
+        self.features_mean = features_mean # 均值
+        self.features_deviation = features_deviation # 标准差
+        self.polynomial_degree = polynomial_degree # 多项式次数
+        self.sinusoid_degree = sinusoid_degree # 正弦函数次数
+        self.normalize_data = normalize_data # 归一化
         
         num_features = self.data.shape[1] # 1表示列，即有多少个特征
-        self.theta = np.zeros((num_features,1))
+        self.theta = np.zeros((num_features,1)) # 初始化参数矩阵
         
-    def train(self,alpha,num_iterations = 500):
+    def train(self, alpha, num_iterations = 500):
         """
                     num_iterations：迭代次数
                     训练模块，执行梯度下降
         """
         cost_history = self.gradient_descent(alpha,num_iterations)
-        return self.theta,cost_history
+        
+        """
+        由于self.theta是类的属性，可以在整个类的实例中访问，所以它并不需要显式地在gradient_descent方法中返回。
+        """
+        return self.theta, cost_history
         
     def gradient_descent(self,alpha,num_iterations):
         """
@@ -41,17 +45,17 @@ class LinearRegression:
         """
         cost_history = [] # 记录每一次损失
         for _ in range(num_iterations):
-            self.gradient_step(alpha)
-            cost_history.append(self.cost_function(self.data,self.labels))
+            self.gradient_step(alpha) # 执行梯度下降
+            cost_history.append(self.cost_function(self.data,self.labels)) # 添加损失
         return cost_history
         
         
-    def gradient_step(self,alpha):    
+    def gradient_step(self, alpha):    
         """
                     一次梯度下降
                     梯度下降参数更新计算方法，注意是矩阵运算
         """
-        num_examples = self.data.shape[0]
+        num_examples = self.data.shape[0] # 样本个数
         prediction = LinearRegression.hypothesis(self.data,self.theta)
         delta = prediction - self.labels # 真实值就是label
         theta = self.theta
@@ -65,7 +69,7 @@ class LinearRegression:
         """
         num_examples = data.shape[0]
         delta = LinearRegression.hypothesis(self.data,self.theta) - labels
-        cost = (1/2)*np.dot(delta.T,delta)/num_examples
+        cost = (1/2)*np.dot(delta.T,delta)/num_examples # 使用均方误差进行损失计算，得出的结果是一个标量
         return cost[0][0]
         
         
@@ -99,7 +103,7 @@ class LinearRegression:
          self.sinusoid_degree,
          self.normalize_data
          )[0]
-         
+        
         predictions = LinearRegression.hypothesis(data_processed,self.theta)
         
         return predictions
